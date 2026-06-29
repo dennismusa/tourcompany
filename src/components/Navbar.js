ff9342e220e4057b6ef4b169fe0d70511c2ab898
dennismusa <97958664+dennismusa@users.noreply.github.com>
2026-06-29 21:28:43 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import i18n from "i18next";
 
-import safarilinklogo from "../assets/safarilinklogo.jpg";
+import safarilinklogo from "../assets/safarilinklogo.png";
 
 function Navbar(){
 
